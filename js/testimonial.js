236e35d89fd92ceaeff649645b9f3f272ee9d635
Sabinaaa <sabinamaka18@gmail.com>
2018-03-23 15:48:13 +0545

    making correction

---
+++ js/testimonial.js
@@ -0,0 +1,11 @@
+$("#client-slider").owlCarousel({
+        items:1,
+        itemsDesktop:[1000,2],
+        itemsDesktopSmall:[990,1],
+        itemsTablet:[768,1],
+        pagination:false,
+        navigation:true,
+        navigationText:["",""],
+        slideSpeed:1000,
+        autoPlay:false
+    });
